dbd8388b6cc7e7068b1a72bc6c649948d264ee3a
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-12 16:00:51 +0100

    sobota

--- projects/onas/src/e2e.ts
+++ projects/onas/src/e2e.ts
@@ -2,6 +2,4 @@
  * Public API Surface of onas
  */
 
-export * from './lib/onas.service';
-export * from './lib/onas.component';
-export * from './lib/onas.module';
+export * from './lib/adapters/primary/ui/onas.component-module'
--- src/app/pages/onas.page-module.ts
+++ src/app/pages/onas.page-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { OnasPage } from './onas.page';
+import { OnasComponentModule } from '../../../projects/onas/src/lib/adapters/primary/ui/onas.component-module';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
@@ -9,7 +10,9 @@ import { OnasPage } from './onas.page';
           path: '',
           component: OnasPage,
         }
-      ])],
+      ]),
+  OnasComponentModule
+],
   	declarations: [OnasPage],
   	providers: [],
   	exports: [] })
