b04bf284fca198b49a11805f6617bb7f74a6d81a
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-13 21:13:23 +0100

    niedziela

--- projects/onas/src/lib/adapters/primary/ui/onas.component.ts
+++ projects/onas/src/lib/adapters/primary/ui/onas.component.ts
@@ -1,8 +1,10 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-onas', templateUrl: './onas.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class OnasComponent {
    autor="Autor";
+ 
    onclick(){
        alert("Patrycja Pietrzak")
        
