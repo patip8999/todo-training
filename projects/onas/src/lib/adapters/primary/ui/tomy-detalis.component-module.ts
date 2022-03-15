2820e44b0667ce3cb6dd05afe928052bbf38254b
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-15 20:35:19 +0100

    Wtorek

---
+++ projects/onas/src/lib/adapters/primary/ui/tomy-detalis.component-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TomyDetalisComponent } from './tomy-detalis.component';
+
+@NgModule({ imports: [CommonModule],
+  	declarations: [TomyDetalisComponent],
+  	providers: [],
+  	exports: [TomyDetalisComponent] })
+export class TomyDetalisComponentModule {
+}
