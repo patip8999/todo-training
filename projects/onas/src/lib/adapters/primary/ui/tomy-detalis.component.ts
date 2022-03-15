96aafe6b64b3fa3cd0f2b585f55e03e38cef7c7b
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-15 22:51:04 +0100

    wtorek

--- projects/onas/src/lib/adapters/primary/ui/tomy-detalis.component.ts
+++
@@ -1,5 +0,0 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({ selector: 'lib-tomy-detalis', templateUrl: './tomy-detalis.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
-export class TomyDetalisComponent {
-}
