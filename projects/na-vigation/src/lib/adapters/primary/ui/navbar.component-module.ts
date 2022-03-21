8bfc5f89a20e2c7a8f5b4f723ee320aac1b4e3c1
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-21 20:28:56 +0100

    poniedzialek

--- projects/na-vigation/src/lib/adapters/primary/ui/navbar.component-module.ts
+++ projects/na-vigation/src/lib/adapters/primary/ui/navbar.component-module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactUsPageModule } from 'src/app/pages/contact-us.page-module';
+import { OnasPageModule } from 'src/app/pages/onas.page-module';
+import { GłownaPageModule } from 'src/app/pages/głowna.page-module';
+import { AutorPageModule } from 'src/app/pages/autor.page-module';
+import { SerialPageModule } from 'src/app/pages/serial.page-module';
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [NavbarComponent],
-  	providers: [],
-  	exports: [NavbarComponent] })
+const routes: Routes = [{ 
+	path: 'onas', 
+	loadChildren: () => OnasPageModule
+  },
+{ 
+	path: 'głowna', 
+	loadChildren: () => GłownaPageModule
+  },
+{ 
+	path: 'autor', 
+	loadChildren: () => AutorPageModule
+  },
+{ 
+	path: 'serial', 
+	loadChildren: () => SerialPageModule
+  },
+{ 
+	path: 'Kontakt', 
+	loadChildren: () => ContactUsPageModule
+  }
+];
+@NgModule({ imports: [CommonModule, RouterModule.forRoot(routes), GłownaPageModule],
+	declarations: [NavbarComponent],
+	providers: [],
+	exports: [NavbarComponent, RouterModule] })
 export class NavbarComponentModule {
 }
