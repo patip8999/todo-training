41f0b729e5344b6548e3e98fe293e2c71588369d
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-22 21:02:49 +0100

    wtorek

--- projects/onas/src/lib/adapters/primary/ui/onas.component.ts
+++ projects/onas/src/lib/adapters/primary/ui/onas.component.ts
@@ -1,19 +1,21 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 import { TomyDTO } from '../../../application/ports/secondary/tomy.dto';
 
 @Component({ selector: 'lib-onas', templateUrl: './onas.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class OnasComponent {
-    tom:TomyDTO={
-        title:" Gra o tron",
-        image:"assets/Images/Tom01.jpg"
-    
-    }
+    tom: Observable<TomyDTO[]> = of([
+        {
+            title: " Gra o tron",
+            image: "assets/Images/Tom01.jpg",
 
-   autor="Autor";
- 
-   onclick(){
-       alert("Patrycja Pietrzak")
-       
-   }
+        }])
+
+    autor = "Autor";
+
+    onclick() {
+        alert("Patrycja Pietrzak")
+
+    }
 }
