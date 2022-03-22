155721f93653d750718ce75a5752d77de807ed3c
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-22 18:24:41 +0100

    wtorek

--- src/app/pages/autor.page-module.ts
+++ src/app/pages/autor.page-module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import { AutorPage } from './autor.page';
 import { OnasComponentModule } from '../../../projects/onas/src/lib/adapters/primary/ui/onas.component-module';
 import { AutorbioComponentModule } from '../../../projects/autor/src/publ/adapters/primary/ui/autorbio.component-module';
-import { NavbarComponentModule } from '../../../projects/na-vigation/src/lib/adapters/primary/ui/navbar.component-module';
+
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
