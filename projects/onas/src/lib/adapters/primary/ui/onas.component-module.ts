77639774bd786269f7e0915c1cf368072cab4b23
patip8999 <100317974+patip8999@users.noreply.github.com>
2022-03-22 17:29:30 +0100

    wtorek

--- projects/onas/src/lib/adapters/primary/ui/onas.component-module.ts
+++ projects/onas/src/lib/adapters/primary/ui/onas.component-module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnasComponent } from './onas.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
-@NgModule({ imports: [CommonModule,ReactiveFormsModule],
+@NgModule({ imports: [CommonModule,ReactiveFormsModule, AppRoutingModule],
   	declarations: [OnasComponent],
   	providers: [],
   	exports: [OnasComponent] })
